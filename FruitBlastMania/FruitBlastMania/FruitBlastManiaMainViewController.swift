01bf137c2afa81e8e8afc7dfa7a9cc296a854a25
MrFool <lordyyap@gmail.com>
2015-02-28 23:20:18 +0800

    Added app icon, changed up images and added some game flow interactions

--- FruitBlastMania/FruitBlastMania/FruitBlastManiaMainViewController.swift
+++ FruitBlastMania/FruitBlastMania/FruitBlastManiaMainViewController.swift
@@ -12,13 +12,6 @@ class FruitBlastManiaMainViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    // do we really need this?
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
     
     override func prefersStatusBarHidden() -> Bool {
